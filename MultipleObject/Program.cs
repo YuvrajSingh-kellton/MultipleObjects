b8ee2998e66da3bd177e5968f2b4c933bd14447f
YuvrajSingh-kellton <105622006+YuvrajSingh-kellton@users.noreply.github.com>
2022-08-11 00:41:26 +0530

    Initial Commit

---
+++ MultipleObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultipleObject
+{
+
+    class Employee
+    {
+
+        string department;
+
+        static void Main(string[] args)
+        {
+
+
+            Employee Yuvraj = new Employee();
+
+
+            sheeran.department = "Development";
+            Console.WriteLine("Sheeran: " + Yuvraj.department);
+
+
+            Employee Rahul = new Employee();
+
+
+            taylor.department = "Content Writing";
+            Console.WriteLine("Taylor: " + Rahul.department);
+
+            Console.ReadLine();
+        }
+    }
+}
